03fc4c743aca9d25a794a8cb9bcf8f7cd82e7519
Mahmoud-Git123 <abdelfa2@vdi-scclab-039.lancs.local>
2023-11-04 21:35:20 +0000

    created a new java file to run/perform the tests

---
+++ Resources/PerformTest.java
@@ -0,0 +1,23 @@
+public class PerformTest implements CollectionTest{
+    
+    //variables
+    private int collectionSize;
+    private int sum;
+    private int searchedValue;
+    
+    //setSize method
+    public void setSize(int size){
+        collectionSize = size;
+    }
+
+    public void runTest(CollectionType type, TestType test, int iterations){
+
+        //Array List
+
+
+        //Linked List
+
+        //Hash Map
+    }
+
+}
