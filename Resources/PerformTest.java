e7c2f605d972b602421103c035d6313c9bcad59b
Mahmoud-Git123 <abdelfa2@vdi-scclab-039.lancs.local>
2023-11-10 05:22:24 +0000

    finalised the search and index methods for hash maps, and test all results, which seem to be as expected

--- Resources/PerformTest.java
+++ Resources/PerformTest.java
@@ -93,7 +93,14 @@ public class PerformTest implements CollectionTest{
     }
 
     public void indexHashMap(HashMap<Integer, Person>map){
-
+        int ID = collectionSize/2;
+        
+        for (Map.Entry<Integer, Person> entry : map.entrySet()) {
+            if (entry.getKey() == map.keySet().toArray()[ID]) {
+                entry.getValue();
+                break;
+            }
+        }
     }
 
     public void search(List<Person> list){
@@ -108,7 +115,10 @@ public class PerformTest implements CollectionTest{
     }
 
     public void searchHashMap(HashMap<Integer, Person>map){
-
+        int ID = collectionSize/2;    
+        if (map.containsKey(ID)){
+            Person p = map.get(ID);
+        }
     }
 
 
