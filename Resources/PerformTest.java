23b2668525b9840471c6e674f6867bd9329e4f93
Mahmoud-Git123 <abdelfa2@vdi-scclab-039.lancs.local>
2023-11-10 05:19:26 +0000

    reworked the code to implement a loop for iterations correctly and finalised add method for all collection types

--- Resources/PerformTest.java
+++ Resources/PerformTest.java
@@ -2,52 +2,22 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Map;
 
 public class PerformTest implements CollectionTest{
-    
-    public PerformTest(){
-        
-    }
-
-
     //variables
     private int collectionSize;
 
     private ArrayList <Person> actualArray;
     private LinkedList <Person> actualLinked;
     private HashMap <Integer, Person> actualHash;
-    
 
-    //Creating and population of Array List
-    private ArrayList<Person> addArray(int collectionSize){
-        ArrayList<Person> arrayList = new ArrayList<Person>();
-        for (int i = 0; i < collectionSize; i++){
-            Person p = new Person("person" + i, 19);
-            arrayList.add(p);
-        }
-        return arrayList;
+    //constructor with no parameters
+    public PerformTest(){
+        actualArray = new ArrayList<>();
+        actualLinked = new LinkedList<>();
+        actualHash = new HashMap<>();
     }
-
-    //Creating and population of Linked List
-    private LinkedList<Person> addLinked(int collectionSize){
-        LinkedList<Person> linkedList = new LinkedList<Person>();
-        for (int i = 0; i < collectionSize; i++){
-            Person p = new Person("person" + i, 19);
-            linkedList.add(p);
-        }
-        return linkedList;
-    }
-
-    //Creating and population of Hash Map
-    private HashMap<Integer, Person> addHash(int collectionSize){
-        HashMap<Integer, Person> hashMap = new HashMap<Integer, Person>();
-        for (int i = 0; i < collectionSize; i++){
-            Person p = new Person("person" + i, 19);
-            hashMap.put(p.hashCode(), p);
-        }
-        return hashMap;
-    }
-    
     
     //setSize method
     public void setSize(int size){
@@ -57,33 +27,25 @@ public class PerformTest implements CollectionTest{
     //ADD TEST TYPE
     public void runTest(CollectionType type, TestType test, int iterations){
 
-        //Array List ADD
-        if (type == CollectionType.ARRAY_LIST){
-            if (test == TestType.ADD){
-                actualArray = addArray(collectionSize);
-            }
-        //Linked list ADD
-        } else if (type == CollectionType.LINKED_LIST){
-            if (test == TestType.ADD){
-                actualLinked = addLinked(collectionSize);
-            }
-
-        //Hash map ADD
-        } else if (type == CollectionType.HASH_MAP){
-            if (test == TestType.ADD){
-                actualHash = addHash(collectionSize);
-            } 
-        }
-
         for (int i = 0; i < iterations; i++){
-            if (test == TestType.INDEX){
+
+            if (test == TestType.ADD){
+                if (type == CollectionType.ARRAY_LIST){
+                    actualArray = addArray(collectionSize);
+                } else if (type == CollectionType.LINKED_LIST){
+                    actualLinked = addLinked(collectionSize);
+                } else if (type == CollectionType.HASH_MAP){
+                    actualHash = addHash(collectionSize);
+                } 
+            }else if (test == TestType.INDEX){
                 if (type == CollectionType.ARRAY_LIST){
                     index(actualArray);
                 } else if (type == CollectionType.LINKED_LIST){
                     index(actualLinked);
                 } else if (type == CollectionType.HASH_MAP){
-
+                    indexHashMap(actualHash);
                 }
+
             } else if (test == TestType.SEARCH){
                 if (type == CollectionType.ARRAY_LIST){
                     search(actualArray);
@@ -94,31 +56,66 @@ public class PerformTest implements CollectionTest{
                 }
             }
         }
-        
     }
 
-    //index method
+    private ArrayList<Person> addArray(int collectionSize){
+        ArrayList<Person> arrayList = new ArrayList<Person>();
+        for (int i = 0; i < collectionSize; i++){
+            Person p = new Person("person" + i, i);
+            arrayList.add(p);
+        }
+        return arrayList;
+    }
+
+    private LinkedList<Person> addLinked(int collectionSize){
+        LinkedList<Person> linkedList = new LinkedList<Person>();
+        for (int i = 0; i < collectionSize; i++){
+            Person p = new Person("person" + i, i);
+            linkedList.add(p);
+        }
+        return linkedList;
+    }
+
+    private HashMap<Integer, Person> addHash(int collectionSize){
+        HashMap<Integer, Person> hashMap = new HashMap<Integer, Person>();
+        for (int i = 0; i < collectionSize; i++){
+            Person p = new Person("person" + i, i);
+            hashMap.put(p.hashCode(), p);
+        }
+        return hashMap;
+    }
+    
     public void index(List<Person> list){
         int index = collectionSize/2;
-        while (index >= 0 && index < collectionSize){
-            Person p = list.get(index);
+        for (int i = 0; i < collectionSize; i++){
+            list.get(index);
         }
     }
 
-    //search method
-    public void search(List<Person> list){
-        String personSearched = ("person" + collectionSize/2);
-        for (int i = 0; i < collectionSize; i++){
-            Person p = list.get(i);
-            if (p.getName().equals(personSearched)){
+    public void indexHashMap(HashMap<Integer, Person>map){
+        int ID = collectionSize/2;
+        
+        for (Map.Entry<Integer, Person> entry : map.entrySet()) {
+            if (entry.getKey() == map.keySet().toArray()[ID]) {
+                entry.getValue();
                 break;
             }
         }
     }
 
-    //search method for hashmap
+    public void search(List<Person> list){
+        String personSearched = ("person" + collectionSize/2);
+        for (int i = 0; i < collectionSize; i++){
+            Person p = list.get(i);
+            if (p.getName() == (personSearched)){
+                break;
+            }
+        }
+        
+    }
+
     public void searchHashMap(HashMap<Integer, Person>map){
-        int ID = collectionSize/2;
+        int ID = collectionSize/2;    
         if (map.containsKey(ID)){
             Person p = map.get(ID);
         }
@@ -126,4 +123,3 @@ public class PerformTest implements CollectionTest{
 
 
 }
-
